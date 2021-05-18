95b4d02b57ca9ab14cfe83f120786f5cbb6785c2
二宁 <12345678>
2021-05-18 14:11:56 +0800

    改成私钥版

--- Exchange/Class1.cs
+++ Exchange/Class1.cs
@@ -6,6 +6,7 @@ using System.Text.RegularExpressions;
 using Wox.Plugin;
 using Newtonsoft.Json.Linq;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Exchange
 {
@@ -18,83 +19,126 @@ namespace Exchange
 
         public List<Result> Query(Query query)
         {
-            if (query.Search.Length < 5) 
+            // 至少输入了两段内容
+            if (query.Terms.Length < 2)
             {
-                // USD 1     所以长度至少是5
-                return new List<Result>();
+                return null;
             }
 
-            string[] keys = query.Search.Split(' ');
-            if (keys.Length == 2 && IsNumeric(keys[1]))
+            string amount = ""; // 金额
+            string baseCode = null; // 源币种代码
+            List<string> targetCode = new List<string>();
+
+            int numberCount = 0; // 数字数量
+            for (int i = 0; i < query.Terms.Length; i++)
             {
-                if (keys[0].Length == 3)
+                string term = query.Terms[i];
+                if (IsNumeric(term)) // 是金额
                 {
-                    return Get(keys[1], keys[0].ToUpper(), "CNY,USD,AUD,CAD,EUR,GBP,HKD,JPY,RUB");
-                }
-            }
-
-            if (keys.Length == 3)
-            {
-                if (keys[0].Length == 3 && keys[1].Length == 3 && IsNumeric(keys[2]))
-                {
-                    return Get(keys[2], keys[0].ToUpper(), keys[1].ToUpper());
-                }
-            }
-
-            return new List<Result>();
-        }
-
-        // 处理内容
-        public List<Result> Get(string number, string sourceCode, string targetCodes)
-        {
-            List<Result> results = new List<Result>();
-            JObject jObject = Request(number, sourceCode, targetCodes);
-
-            if (jObject.Value<String>("code").Equals("0"))
-            {
-                JArray array = jObject.Value<JArray>("data");
-                foreach (JObject obj in array)
-                {
-                    if (obj.Value<Int32>("success") == 1)
+                    if (amount.Length == 0)
                     {
-                        string targetValue = obj.Value<String>("targetValue");
-                        if (targetValue.Contains(".")) 
-                        {
-                            double value = Double.Parse(targetValue);
-                            targetValue = value.ToString("0.00");
-                        }
-                        results.Add(new Result()
-                        {
-                            Title = "   " + number + " " + sourceCode + " = " + targetValue + " " + obj.Value<String>("target"),
-                            SubTitle = "    回车复制结果",
-                            IcoPath = "img/exchange.png",  //相对于插件目录的相对路径
-                            Action = e =>
-                            {
-                                // 处理用户选择之后的操作
-                                Clipboard.SetText(targetValue);
-                                //返回false告诉Wox不要隐藏查询窗体，返回true则会自动隐藏Wox查询窗口
-                                return true;
-                            }
-                        });
+                        amount = term;
+                    }
+                    numberCount++;
+                }
+                else // 是币种代码
+                {
+                    if (term.Length != 3) // 根据观察，所有币种的代码都是3位
+                    {
+                        return null;
+                    }
+                    if (baseCode == null) // 第一个是源币种代码
+                    {
+                        baseCode = term;
+                    }
+                    else 
+                    {
+                        targetCode.Add(term.ToUpper());
                     }
                 }
             }
-            return results;
-        }
+            // 必须只有一个是数字
+            if (numberCount != 1)
+            {
+                return new List<Result>();
+            }
 
-        // 网络请求
-        public JObject Request(string number,string sourceCode,string targetCodes) 
-        {
-            var request = (HttpWebRequest)WebRequest.Create("https://www.erning.cn:10205/base/exchange?money=" + number + "&sourceCode=" + sourceCode + "&targetCodes="+targetCodes);
-            var response = (HttpWebResponse)request.GetResponse();
-            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            return Str2Json(responseString);
+            if (targetCode.Count < 1) // 目标币种代码是空的，添加默认数据
+            {
+                targetCode.Add("CNY");
+                targetCode.Add("USD");
+                targetCode.Add("AUD");
+                targetCode.Add("CAD");
+                targetCode.Add("EUR");
+                targetCode.Add("GBP");
+                targetCode.Add("HKD");
+                targetCode.Add("JPY");
+                targetCode.Add("RUB");
+            }
+
+            return Get(amount, baseCode.ToUpper(), targetCode);
         }
 
         // 是不是数字
         public static bool IsNumeric(string value)
         {
             return Regex.IsMatch(value, @"^[+-]?\d*[.]?\d*$");
+        }
+
+        // 处理内容
+        public List<Result> Get(string number, string baseCode, List<string> targetCodes)
+        {
+            List<Result> results = new List<Result>();
+
+            JObject jObject = Request(baseCode);
+            if (!jObject.Value<String>("result").Equals("success")) 
+            {
+                results.Add(new Result()
+                {
+                    Title = "   " + jObject.Value<String>("error-type"),
+                    SubTitle = "    See detail in provider",
+                    IcoPath = "img/exchange_white.png",  //相对于插件目录的相对路径
+                    Action = e =>
+                    {
+                        // 处理用户选择之后的操作
+                        Process.Start("https://www.exchangerate-api.com/docs/standard-requests");
+                        //返回false告诉Wox不要隐藏查询窗体，返回true则会自动隐藏Wox查询窗口
+                        return true;
+                    }
+                });
+                return results;
+            }
+
+            JObject array = jObject.Value<JObject>("conversion_rates");
+            foreach (string target in targetCodes)
+            {
+                double exchange = Double.Parse(array.Value<string>(target));
+                double result = Double.Parse(number) * exchange;
+                results.Add(new Result()
+                {
+                    Title = "   " + number + " " + baseCode + " = " + result + " " + target,
+                    SubTitle = "    Press enter to copy the result",
+                    IcoPath = "img/exchange_white.png",  //相对于插件目录的相对路径
+                    Action = e =>
+                    {
+                        // 处理用户选择之后的操作
+                        Clipboard.SetText(""+result);
+                        //返回false告诉Wox不要隐藏查询窗体，返回true则会自动隐藏Wox查询窗口
+                        return true;
+                    }
+                });
+            }
+            return results;
+        }
+
+        // 网络请求
+        public JObject Request(string baseCode)
+        {
+            string key = "d4a51cdfd3edb47b406668bb";
+            var request = (HttpWebRequest)WebRequest.Create("https://v6.exchangerate-api.com/v6/" + key + "/latest/" + baseCode);
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return Str2Json(responseString);
         }
 
         // 字符串转json
